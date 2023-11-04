80cd27cd98c3feb10ef940e6137fd5463bb3293a
qbibubi <jakub.sobieraj2137@gmail.com>
2023-11-05 00:33:48 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
+/*
+	1. Select a process - Probably with PID for now
+	2. Loading the DLL into program memory - Know the path of DLL and having access to it
+	3. Get a handle to the process - OpenProcess
+	4. Allocate memory inside address - CreateRemoteThread, WriteProcessMemory
+	5. Load the DLL into the process - LoadLibrary
+*/
+
 #include <iostream>
+#include <Windows.h>
+#include <tlhelp32.h>
 
-// TODO
+int main() 
+{
+	DWORD processId = 0;
+	LPCSTR libraryName = "";
+	
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, processId);
+	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, NU	LL, NULL, 0);
 
 
 
-int main() {
+	CloseHandle(hProcess);
 	return 0;
 }
