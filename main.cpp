f4bc839af7ba6c3d367feac156d89292575e50cb
qbibubi <jakub.sobieraj2137@gmail.com>
2023-11-06 19:52:21 +0100

    Add unabomber.dll

--- main.cpp
+++ main.cpp
@@ -6,35 +6,32 @@
 	5. Load the DLL into the process - LoadLibrary
 */
 
+
 #include <iostream>
-#include <Windows.h>
-#include <tchar.h>
+#include <windows.h>
+
 
 int main() 
-{
-	DWORD processId = 0;
-	DWORD dwSize;
-	const wchar_t* dllPath = L"testdll.dll";
-	
-	HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, FALSE, processId);
-	if (hProcess == NULL) 
-	{
-		CloseHandle(hProcess);
-		return 1;
-	}
+{	
+	DWORD processId{ 12852 }; //temporary id
+	LPCSTR dllPath = "lib//unabomber.dll";
+	SIZE_T dllSize = strlen(dllPath) + 1;
 
-	LPVOID lpRemoteThreadProcess = VirtualAllocEx(hProcess, NULL, _tcslen(dllPath) + 1, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-	if (lpRemoteThreadProcess == NULL)
-	{
-		CloseHandle(hProcess);
-		return 1;
-	}
+	HANDLE hProcess = OpenProcess( PROCESS_ALL_ACCESS , FALSE, processId );
+	LPVOID lpDllPath = VirtualAllocEx( hProcess, NULL, dllSize, MEM_COMMIT , PAGE_READWRITE);
 
+	WriteProcessMemory(hProcess, lpDllPath, reinterpret_cast<LPVOID>(lpDllPath), dllSize, NULL);
 
-	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, NULL, NULL, 0);
+	HANDLE hLoadThread = CreateRemoteThread(
+		hProcess, 
+		NULL, 
+		NULL, 
+		reinterpret_cast<LPTHREAD_START_ROUTINE>(GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA")), lpDllPath, NULL, NULL
+	);
+	std::cout << lpDllPath << std::endl;
+	std::cin.get();
 
-
-
+	VirtualFreeEx(hProcess, lpDllPath, dllSize, MEM_RELEASE);
 	CloseHandle(hProcess);
 	return 0;
 }
