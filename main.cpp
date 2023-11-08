188eaa8bbfd37ba9fe9ab5ff244583cf3c6ea59f
qbibubi <jakub.sobieraj2137@gmail.com>
2023-11-08 21:23:43 +0100

    Create suspended process and inject DLL

--- main.cpp
+++ main.cpp
@@ -1,29 +1,51 @@
-/*
-	1. Select a process - Probably with PID for now
-	2. Loading the DLL into program memory - Know the path of DLL and having access to it
-	3. Get a handle to the process - OpenProcess
-	4. Allocate memory inside address - CreateRemoteThread, WriteProcessMemory
-	5. Load the DLL into the process - LoadLibrary
-*/
-
-
 #include <iostream>
 #include <windows.h>
 
 
 int main() 
 {	
-	DWORD processId = 8308;
-	LPCSTR dllPath = "D:\\dev\\qbinjector\\lib\\unabomber.dll";
+	// Create suspended process of Assault Cube
+	LPCWSTR assaultCube = L"C:\\Program Files (x86)\\AssaultCube 1.3.0.2\\assaultcube.bat";
+	STARTUPINFO startupInfo;
+	PROCESS_INFORMATION processInfo;
+
+	// Set the memory of startupInfo to zero
+	ZeroMemory(&startupInfo, sizeof(startupInfo));
+	startupInfo.cb = sizeof(startupInfo);
+
+	BOOL createProcess = CreateProcessW(
+		assaultCube, 
+		NULL, 
+		NULL, 
+		NULL, 
+		NULL, 
+		CREATE_SUSPENDED, 
+		NULL, 
+		L"C:\\Program Files (x86)\\AssaultCube 1.3.0.2",
+		&startupInfo, 
+		&processInfo
+	);
+
+	if (!createProcess) {
+		MessageBoxW(NULL, L"Failed to create process", L"Error", MB_ICONERROR);
+		return EXIT_FAILURE;
+	}
+
+	// Inject DLL into the suspended process
+	LPCSTR dllPath = "D:\\dev\\qbinjector\\lib\\assaultqb.dll";
 	SIZE_T dllSize = strlen(dllPath) + 1;
+	LPVOID lpDllPath = VirtualAllocEx( processInfo.hProcess, NULL, dllSize, MEM_COMMIT , PAGE_READWRITE);
 
-	HANDLE hProcess = OpenProcess( PROCESS_ALL_ACCESS , FALSE, processId );
-	LPVOID lpDllPath = VirtualAllocEx( hProcess, NULL, dllSize, MEM_COMMIT , PAGE_READWRITE);
-
-	WriteProcessMemory( hProcess, lpDllPath, reinterpret_cast<LPCVOID>( dllPath ), dllSize, NULL );
+	WriteProcessMemory(
+		processInfo.hProcess, 
+		lpDllPath, 
+		reinterpret_cast<LPCVOID>( dllPath ), 
+		dllSize, 
+		NULL
+	);
 
 	HANDLE hLoadThread = CreateRemoteThread(
-		hProcess, 
+		processInfo.hProcess,
 		NULL, 
 		NULL, 
 		reinterpret_cast<LPTHREAD_START_ROUTINE>( GetProcAddress( GetModuleHandleA( "Kernel32.dll" ), "LoadLibraryA" ) ),
@@ -31,10 +53,11 @@ int main()
 		NULL,
 		NULL
 	);
-	std::cout << lpDllPath << std::endl;
-	std::cin.get();
+	std::cout << lpDllPath << " " << processInfo.hProcess << std::endl;
+	MessageBoxW(NULL , L"Sucesfully injected at ", L"qbinjector", MB_ICONINFORMATION);
+	ResumeThread(processInfo.hThread);
 
-	VirtualFreeEx( hProcess, NULL, dllSize, MEM_RELEASE );
-	CloseHandle( hProcess );
+	VirtualFreeEx( processInfo.hProcess, NULL, dllSize, MEM_RELEASE );
+	CloseHandle( processInfo.hProcess );
 	return EXIT_SUCCESS;
 }
