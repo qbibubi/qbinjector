3e71c50bf05b95a70f53d42778807d2c5155310d
qbibubi <jakub.sobieraj2137@gmail.com>
2023-11-10 21:44:54 +0100

    Inject dll into opened window

--- main.cpp
+++ main.cpp
@@ -2,68 +2,67 @@
 #include <windows.h>
 
 
-int main() 
-{	
-	// Create suspended process of Assault Cube
-	LPCWSTR assaultCube = L"C:\\Program Files (x86)\\AssaultCube 1.3.0.2\\assaultcube.bat";
-	STARTUPINFO startupInfo;
-	PROCESS_INFORMATION processInfo;
-
-	// Set the memory of startupInfo and processInfo to zero
-	ZeroMemory( &startupInfo, sizeof(startupInfo) );
-	startupInfo.cb = sizeof(startupInfo);
-	ZeroMemory( &processInfo, sizeof(processInfo) );
-
-	BOOL createProcess = CreateProcessW(
-		assaultCube, 
-		NULL, 
-		NULL, 
-		NULL, 
-		NULL, 
-		NORMAL_PRIORITY_CLASS,
-		NULL, 
-		L"C:\\Program Files (x86)\\AssaultCube 1.3.0.2",
-		&startupInfo, 
-		&processInfo
-	);
-
-	if (!createProcess) {
-		MessageBoxW( NULL, L"Failed to create process", L"Error", MB_ICONERROR );
-		CloseHandle( processInfo.hProcess );
-		return EXIT_FAILURE;
-	}
-
+int main()
+{
+	HWND hWindow = FindWindowA(NULL, "AssaultCube");
+	DWORD processId;
+	GetWindowThreadProcessId(hWindow, &processId);
+	
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
 
 	// Inject DLL into the suspended process
 	LPCSTR dllPath = "D:\\dev\\assaultqb\\assaultqb\\bin\\Win32\\Debug\\assaultqb.dll";
 	SIZE_T dllSize = strlen(dllPath) + 1;
-	LPVOID lpDllPath = VirtualAllocEx( processInfo.hProcess, NULL, dllSize, MEM_COMMIT , PAGE_READWRITE);
+	LPVOID lpDllPath = VirtualAllocEx( hProcess, 0, dllSize, MEM_COMMIT, PAGE_READWRITE );
 
-	WriteProcessMemory(
-		processInfo.hProcess, 
-		lpDllPath, 
-		reinterpret_cast<LPCVOID>( dllPath ), 
-		dllSize, 
-		NULL
-	);
+	if (lpDllPath == NULL)
+	{
+		std::cout << "Dll path is empty" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	if (!WriteProcessMemory(
+		hProcess,
+		lpDllPath,
+		dllPath,
+		dllSize,
+		0)
+		) 
+	{
+		std::cout << "Failed to write process memoery" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	HMODULE hKernel32 = GetModuleHandleA("Kernel32.dll");
+	if (hKernel32 == NULL) 
+	{
+		std::cout << "Handle error, unable to get a handle to Kernel32" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	FARPROC pfnLoadLibraryA = GetProcAddress(hKernel32, "LoadLibraryA");
+	if (pfnLoadLibraryA == NULL) 
+	{
+		std::cout << "Handle error, unable to get the address of LoadLibraryA" << std::endl;
+		return EXIT_FAILURE;
+	}
 
 	HANDLE hLoadThread = CreateRemoteThread(
-		processInfo.hProcess,
-		NULL, 
-		NULL, 
-		reinterpret_cast<LPTHREAD_START_ROUTINE>( GetProcAddress(GetModuleHandleA( "Kernel32.dll" ), "LoadLibraryA") ),
+		hProcess,
+		0,
+		0,
+		reinterpret_cast<LPTHREAD_START_ROUTINE>(pfnLoadLibraryA),
 		lpDllPath,
 		NULL,
 		NULL
 	);
-	std::cout << lpDllPath << " " << processInfo.hProcess << std::endl;
+	std::cout << lpDllPath << std::endl;
 
+	VirtualFreeEx(hProcess, lpDllPath, 0, MEM_RELEASE);
 
-	MessageBoxW(NULL , L"Sucesfully injected at ", L"qbinjector", MB_ICONINFORMATION);
-	VirtualFreeEx( processInfo.hProcess, NULL, 0, MEM_RELEASE );
-	WaitForSingleObject( processInfo.hProcess, INFINITE);
-	CloseHandle( processInfo.hProcess );
-	CloseHandle(processInfo.hThread);
-
+	// Wait for child process to exit
+	WaitForSingleObject(hProcess, INFINITE);
+	CloseHandle(hProcess);
+	
 	return EXIT_SUCCESS;
 }
